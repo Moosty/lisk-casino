7447ab1ab21e73aab9ae62039192cafdbea4dbb2
jurrem <jurre@moosty.com>
2021-07-07 13:19:18 +0200

    ticket

--- frontend/src/components/TopHeader.js
+++ frontend/src/components/TopHeader.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { XIcon } from '@heroicons/react/outline'
+import {useHistory} from "react-router-dom";
 
 export const TopHeader = () => {
+  const history = useHistory()
+
   return (
     <div className="relative bg-indigo-600">
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
@@ -11,6 +14,9 @@ export const TopHeader = () => {
             <span className="block sm:ml-2 sm:inline-block text-yellow-300 font-bold">
                450.877 LSK
 
+            </span>
+            <span onClick={()=> history.push('/lottery')} className="cursor-pointer underline block sm:ml-2 sm:inline-block text-yellow-600 font-bold">
+               WIN
             </span>
           </p>
         </div>
