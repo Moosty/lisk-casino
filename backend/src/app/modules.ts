28f606335df7289c1bcc00e21f648be0627de690
corbifex <sander@moosty.com>
2021-07-07 15:32:03 +0200

    update

--- backend/src/app/modules.ts
+++ backend/src/app/modules.ts
@@ -1,8 +1,10 @@
 import { Application } from 'lisk-sdk';
+import { DiceModule } from "./modules/dice/dice_module";
 import { PokerModule } from "./modules/poker/poker_module";
 import { RngModule } from "./modules/rng/rng_module";
 
 export const registerModules = (app: Application): void => {
     app.registerModule(RngModule);
     app.registerModule(PokerModule);
+    app.registerModule(DiceModule);
 };
---
+++ backend/test/unit/modules/dice/assets/roll_asset.spec.ts
@@ -0,0 +1,40 @@
+import { RollAsset } from '../../../../../src/app/modules/dice/assets/roll_asset';
+
+describe('RollAsset', () => {
+  let transactionAsset: RollAsset;
+
+	beforeEach(() => {
+		transactionAsset = new RollAsset();
+	});
+
+	describe('constructor', () => {
+		it('should have valid id', () => {
+			expect(transactionAsset.id).toEqual(0);
+		});
+
+		it('should have valid name', () => {
+			expect(transactionAsset.name).toEqual('roll');
+		});
+
+		it('should have valid schema', () => {
+			expect(transactionAsset.schema).toMatchSnapshot();
+		});
+	});
+
+	describe('validate', () => {
+		describe('schema validation', () => {
+      it.todo('should throw errors for invalid schema');
+      it.todo('should be ok for valid schema');
+    });
+	});
+
+	describe('apply', () => {
+    describe('valid cases', () => {
+      it.todo('should update the state store');
+    });
+
+    describe('invalid cases', () => {
+      it.todo('should throw error');
+    });
+	});
+});
