9184cc226aee66dae9896c660f74e3cdc0f56c61
corbifex <sander@moosty.com>
2021-07-09 21:37:03 +0200

    final commit

--- backend/src/app/plugins.ts
+++ backend/src/app/plugins.ts
@@ -1,5 +1,10 @@
-import {Application, HTTPAPIPlugin} from 'lisk-sdk';
+/* eslint-disable @typescript-eslint/no-empty-function */
+import {
+  Application,
+  //HTTPAPIPlugin
+} from 'lisk-sdk';
 
+// @ts-expect-error Unused variable error happens here until at least one module is registered
 export const registerPlugins = (app: Application): void => {
-  app.registerPlugin(HTTPAPIPlugin, { loadAsChildProcess: true });
+  // app.registerPlugin(HTTPAPIPlugin, { loadAsChildProcess: true });
 };
