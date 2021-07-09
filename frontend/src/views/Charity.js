e46a8a3974bda9703ae6b7f3c8bc069dfa6f78e4
Raphael Cornelis <raphael@moosty.com>
2021-07-09 13:30:31 +0200

    cursor pointer

--- frontend/src/views/Charity.js
+++ frontend/src/views/Charity.js
@@ -57,13 +57,13 @@ export const Charity = ({account, setModal, filters, visible, userName, hands, c
               src="https://pixnio.com/free-images/people/male-men/drought-affects-health-and-farming-madagascar-725x485.jpg"
               alt=""
             />
-            <div className={["absolute inset-0 bg-gray-600"
+            <div className={["absolute inset-0 bg-gray-600 cursor-pointer"
             ].join(" ")} style={{mixBlendMode: 'multiply'}} aria-hidden="true"/>
           </div>
           <Container className={["w-app  z-50"].join(" ")}>
             <div className="bg-green-500 py-2 px-4 rounded-default font-medium text-13px text-white text absolute top-6 right-5">COMING SOON</div>
             <div className="flex flex-col md:flex-row justify-between space-y-6 py-24">
-              <div className="flex flex-col  md:w-1/2 z-40">
+              <div className="flex flex-col  md:w-1/2 z-40 cursor-pointer">
                 <Typography className="text-themeButtonTextPrimary items-center text-center" type="h1" Element="h1">project</Typography>
               </div>
             </div>
